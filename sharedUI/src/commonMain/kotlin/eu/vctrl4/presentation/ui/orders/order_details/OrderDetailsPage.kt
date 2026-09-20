39822f9af8e56d9bc229a3bc6e0c7cfd1133239a
perigelium <devnull@asperus.com>
2026-09-20 10:59:29 +0200

    Added actualized Readme

--- sharedUI/src/commonMain/kotlin/eu/vctrl4/presentation/ui/orders/order_details/OrderDetailsPage.kt
+++ sharedUI/src/commonMain/kotlin/eu/vctrl4/presentation/ui/orders/order_details/OrderDetailsPage.kt
@@ -13,247 +13,239 @@ import autocontrol.sharedui.generated.resources.*
 import eu.vctrl4.business.datasource.storage.entities.*
 import eu.vctrl4.common.*
 import eu.vctrl4.theme.*
-import androidx.compose.ui.platform.LocalLayoutDirection
 import eu.vctrl4.ui.custom_views.composable.*
 import org.jetbrains.compose.resources.*
 
 @Composable
 fun OrderDetailsPage(
-    pageTitle: String, order: OrderDetails, titleTexts: List<IdNameValueName>
-)
-{
-    val fontFamily = FontFamily(
-	    Font(Res.font.pfbeausanspro_regular, FontWeight.Normal),
-    )
-    val scrollState = rememberScrollState()
+	pageTitle: String, order: OrderDetails, titleTexts: List<IdNameValueName>
+                    ) {
+	val fontFamily = FontFamily(
+		Font(Res.font.pfbeausanspro_regular, FontWeight.Normal),
+	                           )
+	val scrollState = rememberScrollState()
 
-    Surface(
-	    modifier = Modifier.fillMaxSize(),
-	    shape = RoundedCornerShape(12.dp),
-	    border = BorderStroke(1.dp, color = Colors.cl_d8),
-	    color = Colors.white
+	Surface(
+		modifier = Modifier.fillMaxSize(),
+		shape = RoundedCornerShape(12.dp),
+		border = BorderStroke(1.dp, color = Colors.cl_d8),
+		color = Colors.white
 
-    ) {
-        Column(
-            Modifier
-                .fillMaxSize()
-                .verticalScroll(scrollState)
-                .padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box(
-                Modifier
-                    .fillMaxWidth(0.9f)
-                    .height(4.dp)
-                    .background(Colors.cl_69BE28))
+	       ) {
+		Column(
+			Modifier.fillMaxSize().verticalScroll(scrollState).padding(horizontal = 16.dp),
+			horizontalAlignment = Alignment.CenterHorizontally
+		      ) {
+			Box(
+				Modifier.fillMaxWidth(0.9f).height(4.dp).background(Colors.cl_69BE28)
+			   )
 
-            Spacer(modifier = Modifier.height(24.dp))
+			Spacer(modifier = Modifier.height(24.dp))
 
-            order.PlannedAmount?.let {
-                val strAmount = "${(it/10).toInt()} \u20ac"
+			order.PlannedAmount?.let {
+				val strAmount = "${(it / 10).toInt()} \u20ac"
 
-                Text(
-                    text = strAmount,
-                    fontFamily = fontFamily,
-                    fontSize = 26.sp,
-                    maxLines = 1,
-                    color = Colors.cl_2e3b4c)
+				Text(
+					text = strAmount,
+					fontFamily = fontFamily,
+					fontSize = 26.sp,
+					maxLines = 1,
+					color = Colors.cl_2e3b4c
+				    )
 
 
-                Spacer(modifier = Modifier.height(8.dp))
+				Spacer(modifier = Modifier.height(8.dp))
 
-                Text(
-	                text = Res.string.planned_order_cost.asState,
-	                fontSize = 12.sp,
-	                color = Colors.cl_2e3b4c,
-	                fontFamily = fontFamily,
-	                maxLines = 1,
-	                fontWeight = FontWeight.Thin
-                )
-                Spacer(modifier = Modifier.height(24.dp))
-            }
+				Text(
+					text = Res.string.planned_order_cost.asState,
+					fontSize = 12.sp,
+					color = Colors.cl_2e3b4c,
+					fontFamily = fontFamily,
+					maxLines = 1,
+					fontWeight = FontWeight.Thin
+				    )
+				Spacer(modifier = Modifier.height(24.dp))
+			}
 
-            Text(
-                text = pageTitle,
-                fontFamily = fontFamily,
-                fontSize = 14.sp,
-                maxLines = 1,
-                fontWeight = FontWeight.Bold,
-                color = Colors.cl_393854)
+			Text(
+				text = pageTitle,
+				fontFamily = fontFamily,
+				fontSize = 14.sp,
+				maxLines = 1,
+				fontWeight = FontWeight.Bold,
+				color = Colors.cl_393854
+			    )
 
-            Spacer(modifier = Modifier.height(24.dp))
+			Spacer(modifier = Modifier.height(24.dp))
 
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()
-            ) {
-                titleTexts.forEach { nameValue ->
+			Column(
+				horizontalAlignment = Alignment.CenterHorizontally,
+				modifier = Modifier.fillMaxWidth()
+			      ) {
+				titleTexts.forEach { nameValue ->
 
-                    if (nameValue.isChecked == null)
-                    {
-                        TwoStringsColumnItem(titleSubtitle = nameValue)
-                    } else
-                    {
-                        CheckBoxAndTextRow(item = nameValue)
-                    }
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-                Spacer(modifier = Modifier.height(48.dp))
-            }
-        }
-    }
+					if (nameValue.isChecked == null) {
+						TwoStringsColumnItem(titleSubtitle = nameValue)
+					} else {
+						CheckBoxAndTextRow(item = nameValue)
+					}
+					Spacer(modifier = Modifier.height(16.dp))
+				}
+				Spacer(modifier = Modifier.height(48.dp))
+			}
+		}
+	}
 }
 
 @Composable
-private fun CheckBoxAndTextRow(item: IdNameValueName)
-{
-    Row(
-        Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start, verticalAlignment = Alignment.CenterVertically
-    ) {
+private fun CheckBoxAndTextRow(item: IdNameValueName) {
+	Row(
+		Modifier.fillMaxWidth(),
+		horizontalArrangement = Arrangement.Start,
+		verticalAlignment = Alignment.CenterVertically
+	   ) {
 
-        Image(
-            painter = painterResource(resource = if (item.isChecked == true) Res.drawable.ic_chk_white_on_blue else Res.drawable.ic_chk_gray),
-            contentDescription = "is checked",
-            modifier = Modifier.size(24.dp)
-        )
+		Image(
+			painter = painterResource(resource = if (item.isChecked == true) Res.drawable.ic_chk_white_on_blue else Res.drawable.ic_chk_gray),
+			contentDescription = "is checked",
+			modifier = Modifier.size(24.dp)
+		     )
 
-        Spacer(modifier = Modifier.width(16.dp))
+		Spacer(modifier = Modifier.width(16.dp))
 
-        Text(text = item.toString(), fontSize = 14.sp, color = Colors.cl_232323)
-    }
+		Text(text = item.toString(), fontSize = 14.sp, color = Colors.cl_232323)
+	}
 }
 
 @Composable
 fun OrderDetailsTransportationPage(
-    pageTitle: String,
-    order: OrderDetails,
-    titleTexts1: List<IdNameValueName>,
-    titleTexts2: List<IdNameValueName>,
-    titleTexts3: List<IdNameValueName>
-)
-{
-    val fontFamily = FontFamily(
-        Font(Res.font.pfbeausanspro_regular, FontWeight.Normal),
-    )
-    val scrollState = rememberScrollState()
+	pageTitle: String,
+	order: OrderDetails,
+	titleTexts1: List<IdNameValueName>,
+	titleTexts2: List<IdNameValueName>,
+	titleTexts3: List<IdNameValueName>
+                                  ) {
+	val fontFamily = FontFamily(
+		Font(Res.font.pfbeausanspro_regular, FontWeight.Normal),
+	                           )
+	val scrollState = rememberScrollState()
 
-    Surface(
-        modifier = Modifier.fillMaxWidth().verticalScroll(scrollState),
-        shape = RoundedCornerShape(12.dp),
-        border = BorderStroke(1.dp, Colors.cl_d8),
-        color = Colors.white
-    ) {
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box(
-                Modifier
-                    .fillMaxWidth(0.9f)
-                    .height(4.dp)
-                    .background(Colors.cl_69BE28))
+	Surface(
+		modifier = Modifier.fillMaxWidth().verticalScroll(scrollState),
+		shape = RoundedCornerShape(12.dp),
+		border = BorderStroke(1.dp, Colors.cl_d8),
+		color = Colors.white
+	       ) {
+		Column(
+			Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+			horizontalAlignment = Alignment.CenterHorizontally
+		      ) {
+			Box(
+				Modifier.fillMaxWidth(0.9f).height(4.dp).background(Colors.cl_69BE28)
+			   )
 
-            Spacer(modifier = Modifier.height(24.dp))
+			Spacer(modifier = Modifier.height(24.dp))
 
-            order.PlannedAmount?.let {
-	            val strAmount = "${(it/10).toInt()} \u20ac"
+			order.PlannedAmount?.let {
+				val strAmount = "${(it / 10).toInt()} \u20ac"
 
-                Text(
-                    text = strAmount,
-                    fontFamily = fontFamily,
-                    fontSize = 26.sp,
-                    maxLines = 1,
-                    color = Colors.cl_2e3b4c)
+				Text(
+					text = strAmount,
+					fontFamily = fontFamily,
+					fontSize = 26.sp,
+					maxLines = 1,
+					color = Colors.cl_2e3b4c
+				    )
 
 
-                Spacer(modifier = Modifier.height(8.dp))
+				Spacer(modifier = Modifier.height(8.dp))
 
-                Text(
-	                text = Res.string.planned_order_cost.asState,
-	                fontSize = 12.sp,
-	                color = Colors.cl_2e3b4c,
-	                fontFamily = fontFamily,
-	                maxLines = 1,
-	                fontWeight = FontWeight.Thin
-                )
-                Spacer(modifier = Modifier.height(24.dp))
-            }
+				Text(
+					text = Res.string.planned_order_cost.asState,
+					fontSize = 12.sp,
+					color = Colors.cl_2e3b4c,
+					fontFamily = fontFamily,
+					maxLines = 1,
+					fontWeight = FontWeight.Thin
+				    )
+				Spacer(modifier = Modifier.height(24.dp))
+			}
 
-            Text(
-                text = pageTitle,
-                fontFamily = fontFamily,
-                fontSize = 14.sp,
-                maxLines = 1,
-                fontWeight = FontWeight.Bold,
-                color = Colors.cl_393854)
+			Text(
+				text = pageTitle,
+				fontFamily = fontFamily,
+				fontSize = 14.sp,
+				maxLines = 1,
+				fontWeight = FontWeight.Bold,
+				color = Colors.cl_393854
+			    )
 
-            Spacer(modifier = Modifier.height(24.dp))
+			Spacer(modifier = Modifier.height(24.dp))
 
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()
-            ) {
-                titleTexts1.forEach { nameValue ->
+			Column(
+				horizontalAlignment = Alignment.CenterHorizontally,
+				modifier = Modifier.fillMaxWidth()
+			      ) {
+				titleTexts1.forEach { nameValue ->
 
-                    if (nameValue.isChecked == null)
-                    {
-                        TwoStringsColumnItem(titleSubtitle = nameValue)
-                    } else
-                    {
-                        CheckBoxAndTextRow(item = nameValue)
-                    }
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-            }
+					if (nameValue.isChecked == null) {
+						TwoStringsColumnItem(titleSubtitle = nameValue)
+					} else {
+						CheckBoxAndTextRow(item = nameValue)
+					}
+					Spacer(modifier = Modifier.height(16.dp))
+				}
+			}
 
-            if(titleTexts2.isNotEmpty())
-            {
-                TwoStringsColumnList(titleTexts = titleTexts2)
-            }
+			if (titleTexts2.isNotEmpty()) {
+				TwoStringsColumnList(titleTexts = titleTexts2)
+			}
 
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()
-            ) {
-                titleTexts3.forEach { nameValue ->
+			Column(
+				horizontalAlignment = Alignment.CenterHorizontally,
+				modifier = Modifier.fillMaxWidth()
+			      ) {
+				titleTexts3.forEach { nameValue ->
 
-                    if (nameValue.isChecked == null)
-                    {
-                        TwoStringsColumnItem(titleSubtitle = nameValue)
-                    } else
-                    {
-                        CheckBoxAndTextRow(item = nameValue)
-                    }
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-            }
-            Spacer(modifier = Modifier.height(64.dp))
-        }
-    }
+					if (nameValue.isChecked == null) {
+						TwoStringsColumnItem(titleSubtitle = nameValue)
+					} else {
+						CheckBoxAndTextRow(item = nameValue)
+					}
+					Spacer(modifier = Modifier.height(16.dp))
+				}
+			}
+			Spacer(modifier = Modifier.height(64.dp))
+		}
+	}
 }
 
 @Preview()
 @Composable
-fun OrderDetailsPreview()
-{
-    val order = OrderDetails()
-    order.OrderId = "2222222222222"
-    order.Number = "38476039460386"
-    order.VehicleName = "03843046"
-    order.CreateDate = "2024-12-12T10:05:00"
-    order.RentStartDate = "2024-12-12T11:05:00"
-    order.OrderType = 'C'
-    order.Priority = 'L'
-    order.ConsumerType = "Mob"
-    order.PlannedAmount = 9999f
+fun OrderDetailsPreview() {
+	val order = OrderDetails()
+	order.OrderId = "2222222222222"
+	order.Number = "38476039460386"
+	order.VehicleName = "03843046"
+	order.CreateDate = "2024-12-12T10:05:00"
+	order.RentStartDate = "2024-12-12T11:05:00"
+	order.OrderType = 'C'
+	order.Priority = 'L'
+	order.ConsumerType = "Mob"
+	order.PlannedAmount = 9999f
 
-    val idName = IdNameValueName()
-    idName.Name = Res.string.name.asState
-    idName.valueName = Res.string.value.asState
-    idName.isChecked = false
+	val idName = IdNameValueName()
+	idName.Name = Res.string.name.asState
+	idName.valueName = Res.string.value.asState
+	idName.isChecked = false
 
-    val idName1 = IdNameValueName()
-    idName1.Name = Res.string.name.asState
-    idName1.valueName = Res.string.value.asState
+	val idName1 = IdNameValueName()
+	idName1.Name = Res.string.name.asState
+	idName1.valueName = Res.string.value.asState
 
-    OrderDetailsPage(pageTitle = Res.string.additional_information_caps.asState, order = order, titleTexts = listOf(idName, idName1))
+	OrderDetailsPage(
+		pageTitle = Res.string.additional_information_caps.asState,
+		order = order,
+		titleTexts = listOf(idName, idName1)
+	                )
 }
--- sharedUI/src/commonMain/kotlin/eu/vctrl4/presentation/ui/orders/order_details/OrderDetailsScreen.kt
+++ sharedUI/src/commonMain/kotlin/eu/vctrl4/presentation/ui/orders/order_details/OrderDetailsScreen.kt
@@ -23,111 +23,140 @@ import org.jetbrains.compose.resources.*
 val stepsCount: Int = BOTTOM_APP_BAR_NAV_ITEMS.size - 1
 
 @Composable
-fun OrderDetailsScreen(order: OrderDetails, titleTexts:List<IdNameValueName>, titleTextsTranspTop:List<IdNameValueName>,
-                       titleTextsTranspOptionTypes:List<IdNameValueName>, titleTextsTranspBottom:List<IdNameValueName>,
-                       onBackPressed:() -> Unit, onPerformAction:(String) -> Unit, navigate:(String) -> Unit)
-{
-    var curStepNumber by remember { mutableIntStateOf(0)}
-    val remSelItemIndex: MutableIntState = remember { mutableIntStateOf(curStepNumber)}
-    var forwardBackward by remember { mutableStateOf(true)}
-    
-    val remStrSubtitle: MutableState<String> = remember { mutableStateOf(BOTTOM_APP_BAR_NAV_ITEMS.get(remSelItemIndex.intValue).title) }
-    val stFabImageVector: MutableState<ImageVector> = remember { mutableStateOf(Icons.AutoMirrored.Default.ArrowForward) }
+fun OrderDetailsScreen(
+	order: OrderDetails,
+	titleTexts: List<IdNameValueName>,
+	titleTextsTranspTop: List<IdNameValueName>,
+	titleTextsTranspOptionTypes: List<IdNameValueName>,
+	titleTextsTranspBottom: List<IdNameValueName>,
+	onBackPressed: () -> Unit,
+	onPerformAction: (String) -> Unit,
+	navigate: (String) -> Unit
+                      ) {
+	var curStepNumber by remember { mutableIntStateOf(0) }
+	val remSelItemIndex: MutableIntState = remember { mutableIntStateOf(curStepNumber) }
+	var forwardBackward by remember { mutableStateOf(true) }
 
-    Surface(modifier = Modifier.fillMaxSize(), shape = RectangleShape, Colors.white) {
-        Column(
-            modifier = Modifier.fillMaxSize().background(Colors.cl_white), horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            val titleText = if (order.Number != null) "No. " + order.Number else Res.string.order.asState
+	val remStrSubtitle: MutableState<String> =
+		remember { mutableStateOf(BOTTOM_APP_BAR_NAV_ITEMS.get(remSelItemIndex.intValue).title) }
+	val stFabImageVector: MutableState<ImageVector> =
+		remember { mutableStateOf(Icons.AutoMirrored.Default.ArrowForward) }
 
-            CenteredAppBar(
-                onBackBtnClick = { onBackPressed() },
-                backColorId = Colors.cl_00549F,
-                titleTxt = titleText,
-                titleColorId = Colors.white,
-                orderState = order.State ?: "",
-                onActionInvoked = { onPerformAction(it) })
+	Surface(modifier = Modifier.fillMaxSize(), shape = RectangleShape, Colors.white) {
+		Column(
+			modifier = Modifier.fillMaxSize().background(Colors.cl_white),
+			horizontalAlignment = Alignment.CenterHorizontally
+		      ) {
+			val titleText =
+				if (order.Number != null) "No. " + order.Number else Res.string.order.asState
 
-            order.State?.let {
-                Row(
-                    modifier = Modifier.fillMaxWidth().background(Colors.cl_00549F),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Icon(
-	                    painter = painterResource(Res.drawable.ic_done),
-	                    contentDescription = Res.string.order_status.asState,
-	                    modifier = Modifier.size(24.dp),
-	                    tint = Colors.white
-                    )
-                    Spacer(modifier = Modifier.width(16.dp))
+			CenteredAppBar(
+				onBackBtnClick = { onBackPressed() },
+				backColorId = Colors.cl_00549F,
+				titleTxt = titleText,
+				titleColorId = Colors.white,
+				orderState = order.State ?: "",
+				onActionInvoked = { onPerformAction(it) })
 
-                    Text(
-                        text = ORDER_STATES_MAP.get(order.State)?:"", color = Colors.white, fontSize = 14.sp, maxLines = 1
-                    )
-                }
-                Box(
-                    modifier = Modifier.height(20.dp).fillMaxWidth().background(Colors.cl_00549F)
-                )
-            }
+			order.State?.let {
+				Row(
+					modifier = Modifier.fillMaxWidth().background(Colors.cl_00549F),
+					verticalAlignment = Alignment.CenterVertically,
+					horizontalArrangement = Arrangement.Center
+				   ) {
+					Icon(
+						painter = painterResource(Res.drawable.ic_done),
+						contentDescription = Res.string.order_status.asState,
+						modifier = Modifier.size(24.dp),
+						tint = Colors.white
+					    )
+					Spacer(modifier = Modifier.width(16.dp))
 
-            Scaffold(modifier = Modifier.background(color = Colors.white).fillMaxSize(), floatingActionButton = { // .nestedScroll(scrollBehavior.nestedScrollConnection)
+					Text(
+						text = ORDER_STATES_MAP.get(order.State) ?: "",
+						color = Colors.white,
+						fontSize = 14.sp,
+						maxLines = 1
+					    )
+				}
+				Box(
+					modifier = Modifier.height(20.dp).fillMaxWidth().background(Colors.cl_00549F)
+				   )
+			}
 
-                Box() {
-                    FloatingActionButton(
-	                    onClick = {
-                            if (forwardBackward && curStepNumber < stepsCount - 1) curStepNumber++
-                            else if (!forwardBackward && curStepNumber > 0) curStepNumber--
-                            if (curStepNumber == stepsCount - 1 || curStepNumber == 0) forwardBackward = !forwardBackward
+			Scaffold(
+				modifier = Modifier.background(color = Colors.white).fillMaxSize(),
+			         floatingActionButton = { // .nestedScroll(scrollBehavior.nestedScrollConnection)
 
-                            navigate(BOTTOM_APP_BAR_NAV_ITEMS.get(curStepNumber).name)
+				         Box() {
+					         FloatingActionButton(
+						         onClick = {
+							         if (forwardBackward && curStepNumber < stepsCount - 1) curStepNumber++
+							         else if (!forwardBackward && curStepNumber > 0) curStepNumber--
+							         if (curStepNumber == stepsCount - 1 || curStepNumber == 0) forwardBackward =
+								         !forwardBackward
 
-                            remStrSubtitle.value = BOTTOM_APP_BAR_NAV_ITEMS.get(curStepNumber).title
-                            remSelItemIndex.intValue = curStepNumber
-                            stFabImageVector.value = if (forwardBackward) Icons.AutoMirrored.Default.ArrowForward else Icons.AutoMirrored.Default.ArrowBack
-                        },
-	                    shape = CircleShape,
-	                    modifier = Modifier.align(Alignment.Center).size(56.dp).offset(y = 40.dp),
-	                    containerColor = Colors.cl_00549F,
-	                    contentColor = Color.White,
-	                    elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
-                    ) {
-                        Icon(
-                            imageVector = stFabImageVector.value, contentDescription = null, modifier = Modifier.size(24.dp)
-                        )
-                    }
-                }
-            }, floatingActionButtonPosition = FabPosition.End, content = { innerPadding ->
+							         navigate(BOTTOM_APP_BAR_NAV_ITEMS.get(curStepNumber).name)
 
-                Column(
-                    modifier = Modifier.padding(top = innerPadding.calculateTopPadding(), bottom = innerPadding.calculateTopPadding() + 60.dp).fillMaxSize()
-                ) {
-                    if ("nav_transportation" == BOTTOM_APP_BAR_NAV_ITEMS.get(curStepNumber).name)
-                    {
-	                    OrderDetailsTransportationPage(
-		                    pageTitle = remStrSubtitle.value,
-		                    order = order,
-		                    titleTexts1 = titleTextsTranspTop,
-		                    titleTexts2 = titleTextsTranspOptionTypes,
-		                    titleTexts3 = titleTextsTranspBottom
-	                                                  )
-                    } else
-                    {
-	                    OrderDetailsPage(remStrSubtitle.value, order, titleTexts)
-                    }
-                }
-            }, bottomBar = {
-                BottomAppBarOrderDetails(
-                    items = BOTTOM_APP_BAR_NAV_ITEMS, remSelItemIndex = remSelItemIndex, onNavigationItemClick = { route ->
-                        navigate(route)
-                        curStepNumber = BOTTOM_APP_BAR_NAV_ITEMS.indexOf(
-                            (BOTTOM_APP_BAR_NAV_ITEMS.firstOrNull { it.name == route }) ?: 0
-                        )
-                        remStrSubtitle.value = BOTTOM_APP_BAR_NAV_ITEMS.get(curStepNumber).title
-                    })
-            },
-                     contentWindowInsets = WindowInsets(0,0,0,0)
-					)
-        }
-    }
+							         remStrSubtitle.value =
+								         BOTTOM_APP_BAR_NAV_ITEMS.get(curStepNumber).title
+							         remSelItemIndex.intValue = curStepNumber
+							         stFabImageVector.value =
+								         if (forwardBackward) Icons.AutoMirrored.Default.ArrowForward else Icons.AutoMirrored.Default.ArrowBack
+						         },
+						         shape = CircleShape,
+						         modifier = Modifier.align(Alignment.Center).size(56.dp)
+							         .offset(y = 40.dp),
+						         containerColor = Colors.cl_00549F,
+						         contentColor = Color.White,
+						         elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation(),
+					                             ) {
+						         Icon(
+							         imageVector = stFabImageVector.value,
+							         contentDescription = null,
+							         modifier = Modifier.size(24.dp)
+						             )
+					         }
+				         }
+			         },
+			         floatingActionButtonPosition = FabPosition.End,
+			         content = { innerPadding ->
+
+				         Column(
+					         modifier = Modifier.padding(
+						         top = innerPadding.calculateTopPadding(),
+						         bottom = innerPadding.calculateTopPadding() + 60.dp
+					                                    ).fillMaxSize()
+				               ) {
+					         if ("nav_transportation" == BOTTOM_APP_BAR_NAV_ITEMS.get(curStepNumber).name) {
+						         OrderDetailsTransportationPage(
+							         pageTitle = remStrSubtitle.value,
+							         order = order,
+							         titleTexts1 = titleTextsTranspTop,
+							         titleTexts2 = titleTextsTranspOptionTypes,
+							         titleTexts3 = titleTextsTranspBottom
+						                                       )
+					         } else {
+						         OrderDetailsPage(remStrSubtitle.value, order, titleTexts)
+					         }
+				         }
+			         },
+			         bottomBar = {
+				         BottomAppBarOrderDetails(
+					         items = BOTTOM_APP_BAR_NAV_ITEMS,
+					         remSelItemIndex = remSelItemIndex,
+					         onNavigationItemClick = { route ->
+						         navigate(route)
+						         curStepNumber = BOTTOM_APP_BAR_NAV_ITEMS.indexOf(
+							         (BOTTOM_APP_BAR_NAV_ITEMS.firstOrNull { it.name == route })
+								         ?: 0
+						                                                         )
+						         remStrSubtitle.value =
+							         BOTTOM_APP_BAR_NAV_ITEMS.get(curStepNumber).title
+					         })
+			         },
+			         contentWindowInsets = WindowInsets(0, 0, 0, 0)
+			        )
+		}
+	}
 }
